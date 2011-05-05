a9d1da4b82fe7fe0279e1dfbe7a332c8a198c185
Ryan Taylor <ryanmt@byu.net>
2011-05-04 19:20:35 -0600

    almost there...

--- lib/tagging_project.rb
+++ lib/tagging_project.rb
@@ -2,40 +2,94 @@
 
 require 'nokogiri'
 
+def cleanup_modification_array(array)
+	outs = []
+	array.each_index do |i|
+		if array[i][0] == array[i-1][0] 
+			if array[i][1] == array[i-1][1]
+				array[i][2] << array[i-1][2]
+				array[i-1] = nil
+			end
+		end
+		array[i][2] = array[i][2].flatten
+		outs << array[i]
+	end
+	outs
+end
+def upcaser(sequence, test_match)
+	regex = test_match
+	sequence.downcase.gsub(/#{regex.downcase}/, "#{regex.upcase}")
+end
+def bolder(sequence)
+	sequence.gsub(/([A-Z]+)/, '<span id="bold">\1</span>')
+end
+
+def colorify(sequence)
+	sequence.gsub(/([A-Z]+)/, '<span id="colorify">\1</span>')
+end	
+
+
+
 if ARGV.size < 1
-	puts "Usage: #{File.basename(__FILE__)} 1.pepxml 2.pepxml ... ?.pepxml "
+	puts "Usage: #{File.basename(__FILE__)} Database.fasta 1.pepxml 2.pepxml ... ?.pepxml "
 	puts "Output: 1.coverage 2.coverage ... ?.coverage"
 	exit
 end
+require 'bio'
+#fasta = Bio::FlatFile.auto(ARGV.shift)
 FASTA = '/home/ryanmt/lab/DB/uni_bovin_var_100518_fwd.fasta'
-fasta = File.open(FASTA, 'r')
+fasta = Bio::FlatFile.auto(FASTA)
+prot_db = Hash.new { |h,k| h[k] = [] }
+pep_db = Hash.new { |h,k| h[k] = [] }
+fasta.each_entry do |entry|
+	prot_db[entry.accession.to_sym] << entry.seq
+	pep_db[entry.seq.to_sym] << entry.accession
+end
+pep_db.each {|k,v| puts v if v.length > 10}
 ARGV.each do |file|
 	doc = Nokogiri.XML(File.open(file))
 	top_hits = doc.xpath('//xmlns:search_hit[@hit_rank="1"]')
-	proteins = []
-	pep_centric = []
-	amidinated = top_hits.map do |hit| 
+	proteins = Hash.new { |h,k| h[k] = [] }
+	pep_centric = Hash.new { |h,k| h[k] = [] }
+	amidinated = []; non_amidinated = []
+	top_hits.each do |hit| 
 		prot = hit.to_s[/protein="(\w*?)"/,1]
 		peptide = hit.to_s[/peptide="([A-Z]*?)"/,1]
-		proteins << [prot, peptide]
-		amidination_site = hit.to_s.slice(/mod_aminoacid_mass\sposition="(\d)"\smass="169.121512"/,1)
-		[prot, peptide,[amidination_site]]
-	end
-	#amidinated = doc.xpath("//xmlns:search_hit[@hit_rank='1']/modification_info/mod_aminoacid_mass")#[@mass='169.121512']")
-	p top_hits.size
-	p amidinated.size
-	amidinated = amidinated.sort_by{|a| a[1]}
-	outs = []
-	amidinated.each_index do |i|
-		if amidinated[i][0] == amidinated[i-1][0] 
-			if amidinated[i][1] == amidinated[i-1][1]
-				amidinated[i][2] << amidinated[i-1][2]
-				amidinated[i-1] = nil
-			end
+		next if peptide.nil? or prot.nil?
+		amidination_site = hit.to_s.scan(/mod_aminoacid_mass\sposition="(\d*?)"\smass="169.121512"/)
+		pep_centric[peptide.to_sym] << prot
+		proteins[prot.to_sym] << peptide
+		if amidination_site.empty?
+			non_amidinated << [prot, peptide,[amidination_site]]
+		else
+			amidinated << [prot, peptide,[amidination_site]]
 		end
-		amidinated[i][2]= amidinated[i][2].flatten.compact.uniq
-		outs << amidinated[i]
 	end
-p outs
+	amidinated = amidinated.sort_by{|a| a[1]}
+	amid_outs = []; non_amid_outs = []
+	non_amid_outs, amid_outs = [non_amidinated, amidinated].map do |array|
+		cleanup_modification_array(array)
+	end
+	p amid_outs.length
+	p non_amid_outs.length
+
+######################################################################
+#	outputing 
+	# pep_db vs pep_centric
+	# prot_db vs proteins
+	output = []
+	output << "HEADER: \nACCESSION: example\nModified:Unmodified"
+	proteins.each do |key, value|
+		sequence = prot_db[key]
+		if sequence.length > 1
+			output << "Protein: #{key} failed because of multiple prot_db sequences" 
+			next
+		end
 		
+
+
 end
+
+
+# => [:definition, :definition=, :data, :data=, :entry_overrun, :entry, :query, :fasta, :blast, :seq, :comment, :length, :naseq, :nalen, :aaseq, :aalen, :to_biosequence, :to_seq, :identifiers, :entry_id, :gi, :accession, :accessions, :acc_version, :locus, :tags, :exists?, :get, :fetch]
+
--- spec/tagging_project_spec.rb
+++ spec/tagging_project_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe "TaggingProject" do
-  it "fails" do
-    should.flunk "hey buddy, you should probably rename this file and start specing for real"
-  end
+	it 'upcases the string' do 
+		seq = "YVSSKALQRQHSEGAAGKAPCILPIIENGK"
+		test = "HSEGAAGK"
+		upcaser(seq, test).should.equal "yvsskalqrqhseGAAGKAPCilpiiengk"
+	end
+	it 'bolds a sequence' do 
+		seq = "yvsskalqrqhseGAAGKAPCilpiiengk"
+		bolder(seq).should.equal "YVSSKALQRQ<span>HSEGAAGK</span>APCILPIIENGK"
+	end
+	it '"colorifies" a sequence' do 
+		seq = "YVSSKALQRQHSEGAAGKAPCILPIIENGK"
+		test2 = "GAAGKAPC"
+		seq2 = upcaser(seq, test2)
+		colorify(seq2).should.equal "yvsskalqrqhse<span id=\"colorify\">GAAGKAPC</span>ilpiiengk"
+	end
 end
