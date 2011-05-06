5a8bf9a29372f62706cc3a57c0b63c49b0e3d9c8
Ryan Taylor <ryanmt@byu.net>
2011-05-06 14:47:37 -0600

    Works!!!!

--- lib/tagging_project.rb
+++ lib/tagging_project.rb
@@ -1,42 +1,20 @@
 #!/usr/bin/env ruby
 
-require 'nokogiri'
-
-def cleanup_modification_array(array)
-	outs = []
-	array.each_index do |i|
-		if array[i][0] == array[i-1][0] 
-			if array[i][1] == array[i-1][1]
-				array[i][2] << array[i-1][2]
-				array[i-1] = nil
-			end
-		end
-		array[i][2] = array[i][2].flatten
-		outs << array[i]
-	end
-	outs
-end
 def upcaser(sequence, test_match, position_arr = nil)
-p position_arr
-p sequence.class
-p test_match	
-	if position_arr.class == Array
+	if position_arr.class == NilClass
+	elsif position_arr.class == Array and not position_arr.empty?
 		regex = test_match 
-		if position_arr.empty?
-			regex = test_match
-			sequence.gsub(/#{regex}/i, "#{regex.upcase}")
-		else
-			position_arr.each do |i|
-				a =  sequence.rpartition(/#{regex}/i)
-				sequence = a.shift + a.first[i]=a.first[i].upcase + a.last
-				p sequence
-				sequence
-			end
+		position_arr.flatten.each do |i|
+			a =  sequence.to_s.rpartition(/#{regex}/i)
+			a[1][i.to_i-1] = a[1][(i.to_i-1)].upcase unless a[1][i.to_i-1].nil? || a[1][i.to_i-1].empty?
+			sequence = a.shift + a.first + a.last 
 		end
 	else
 		regex = test_match
-		sequence.gsub(/#{regex}/i, "#{regex.upcase}")
+		sequence = sequence.gsub(/#{regex}/i, "#{regex.upcase}")
 	end
+#	puts "Sequence: #{sequence}"
+	sequence
 end
 def bolder(sequence)
 	sequence.gsub(/([A-Z]+)/, '<span id="bold">\1</span>')
@@ -57,6 +35,7 @@ if ARGV.size < 1
 	exit
 end
 require 'bio'
+require 'nokogiri'
 #fasta = Bio::FlatFile.auto(ARGV.shift)
 FASTA = '/home/ryanmt/lab/DB/uni_bovin_var_100518_fwd.fasta'
 fasta = Bio::FlatFile.auto(FASTA)
@@ -66,7 +45,7 @@ fasta.each_entry do |entry|
 	prot_db[entry.accession.to_sym] << entry.seq
 	pep_db[entry.seq.to_sym] << entry.accession
 end
-pep_db.each {|k,v| puts v if v.length > 10}
+
 ARGV.each do |file|
 	doc = Nokogiri.XML(File.open(file))
 	top_hits = doc.xpath('//xmlns:search_hit[@hit_rank="1"]')
@@ -83,17 +62,18 @@ ARGV.each do |file|
 		if amidination_site.empty?
 			proteins[prot.to_sym] << ProteinEvidence.new(prot, peptide, nil, amidination_site)
 		else
-			proteins[prot.to_sym] << ProteinEvidence.new(prot, peptide, amidination_site, nil)
+			proteins[prot.to_sym] << ProteinEvidence.new(prot, peptide, amidination_site, [])
 		end
 	end
 ######################################################################
 #	outputing 
 	# pep_db vs pep_centric
 	# prot_db vs proteins
+	output = []
 	ProteinModMap = Struct.new(:accession, :modified_seq, :observed_norm_seq, :output)
 	prot_mods = proteins.map do |key, evidence_structs_array|
-		p key
-		p evidence_structs_array
+#		p key
+#		p evidence_structs_array
 		output_arr = []
 		sequence = prot_db[key.to_sym]
 		if sequence.length > 1
@@ -101,30 +81,37 @@ ARGV.each do |file|
 			output_arr << "Protein: #{key} failed because of multiple prot_db sequences"
 			output_arr << "============"
 		else
-			output_arr << "============"
 			output_arr << "ACCESSION: #{key}"
-			output_arr << "Modified:"
+			output_arr << "#{evidence_structs_array}"
 			tmp = sequence.first.downcase
 			modified_seq = tmp
 			observed_norm_seq = tmp
 			evidence_structs_array.each do |evidence_struct| # 	ProteinEvidence = Struct.new(:accession, :peptide, :modification_arr, :evidence_arr )
 				modified_seq = upcaser(modified_seq, evidence_struct.peptide, evidence_struct.modification_arr)
-				puts "OBS: #{observed_norm_seq}"
+				#puts "OBS: #{observed_norm_seq}"
 				observed_norm_seq = upcaser(observed_norm_seq, evidence_struct.peptide, evidence_struct.evidence_arr)
 			end
+			output_arr << "Modified:"
 			output_arr << modified_seq 
 			output_arr << "Observed:"
 			output_arr << observed_norm_seq
-		puts output_arr 
-		abort
 		end
 
 		ProteinModMap.new(key, modified_seq, observed_norm_seq, output_arr)
 	end
-output_arr << "HEADER: \nACCESSION: example\nModified:Unmodified"
+output << "HEADER: \nACCESSION: example\nModified:Unmodified\n\n\n------------\n\t"
 #puts output_arr
-end
+prot_mods.each {|prot| output << prot.output.join("\n\t")}
+outstring = output.join("\n")
+#p prot_mods
 
+
+
+File.open('tmp.txt','w'){|f|
+	f.print outstring
+}
+end
+	
 
 # => [:definition, :definition=, :data, :data=, :entry_overrun, :entry, :query, :fasta, :blast, :seq, :comment, :length, :naseq, :nalen, :aaseq, :aalen, :to_biosequence, :to_seq, :identifiers, :entry_id, :gi, :accession, :accessions, :acc_version, :locus, :tags, :exists?, :get, :fetch]
 
